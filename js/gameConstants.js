0336b92c05df48f8032c5bc10983edc3c9f4844b
adriankulik <adriankulik93@gmail.com>
2023-02-26 22:42:37 +0100

    added more interactivity

--- js/gameConstants.js
+++ js/gameConstants.js
@@ -1,13 +1,13 @@
 // game constants
 let elapsedTime = 0;
-let howManySecoundsToAddFlowers = 3;
-let howManyMetersToAddFlowers = 15;
+let howManySecoundsToAddMeshes = 2;
+let howManyMetersToAddMeshes = 5;
 let calledTimeTemp = false; // this variable allows me to get into the "elapsedtime if" in the animate function (gameloop) only once, not on every screen refresh that matches the modulo operand
 let calledDistanceTemp = false; // same as above, only we consider the traveled distance, not the elapsed time
 let elapsedTimeDelta = 0;
-let howManyPlanesExistOnStart = 20;
-let maxPlanesAmount = 100;
-let planeArray = []; // here we will store all the planes with floral textures on them
-let planeShadowArray = [];
-let islandArray = [];
-let howManyPlanesAreAddedAndRemoved = 4;
+let howManyMeshesExistOnStart = 5;
+let maxMeshesAmount = 25;
+let meshArray = []; // here we will store all the planes with floral textures on them
+// let planeShadowArray = [];
+// let islandArray = [];
+let howManyMeshesAreAddedAndRemoved = 4;
--- js/visualiseMicrophone.js
+++ js/visualiseMicrophone.js
@@ -13,7 +13,7 @@ function main() {
     75,
     window.innerWidth / window.innerHeight,
     0.01,
-    1000
+    10000
   );
   camera.position.set(0, 0, 40);
   scene.add(camera);
@@ -32,7 +32,7 @@ function main() {
   });
 
   // icosahedron mesh
-  const icosahedronGeometry = new THREE.IcosahedronGeometry(10, 3);
+  const icosahedronGeometry = new THREE.IcosahedronGeometry(10, 1);
   const icosahedronMaterial = new THREE.MeshStandardMaterial({
     color: 0xffffff,
     roughness: 1,
@@ -43,18 +43,6 @@ function main() {
   const icosahedron1 = new THREE.Mesh(icosahedronGeometry, icosahedronMaterial);
   icosahedron1.position.z = -20;
   scene.add(icosahedron1);
-
-  // torus knot mesh
-  const torusKnotGeometry = new THREE.TorusKnotGeometry(10, 3, 100, 16);
-  const torusKnotmaterial = new THREE.MeshStandardMaterial({
-    color: 0xffffff,
-    roughness: 1,
-    metalness: 0.9,
-    emissive: 0x000000,
-    flatShading: 1,
-  });
-  const torusKnot1 = new THREE.Mesh(torusKnotGeometry, torusKnotmaterial);
-  scene.add(torusKnot1);
 
   // light
   const light = new THREE.PointLight(0xffffff, 2, 10000);
@@ -67,6 +55,38 @@ function main() {
   effect.domElement.style.color = "white";
   effect.domElement.style.backgroundColor = "black";
   document.body.appendChild(effect.domElement);
+
+  // function to make a torusKnot
+  let temp;
+  function makeOneKnot(x, y, z, size) {
+    const torusKnotGeometry = new THREE.TorusKnotGeometry(size, 3, 100, 16);
+    const torusKnotmaterial = new THREE.MeshStandardMaterial({
+      color: 0xffffff,
+      roughness: 1,
+      metalness: 0.9,
+      emissive: 0x000000,
+      flatShading: 1,
+    });
+    const torusKnot1 = new THREE.Mesh(torusKnotGeometry, torusKnotmaterial);
+    torusKnot1.position.set(x, y, z);
+    return torusKnot1;
+  }
+
+  // Mesh Array
+  function createMeshes(amount, distance) {
+    for (let i = 0; i < amount; i++) {
+      let x = getRandomInt(25) - 12.5 + distance;
+      let y = getRandomInt(25) - 12.5;
+      let z = getRandomInt(1000) * -1;
+      let size = getRandomInt(10);
+      let temp = makeOneKnot(x, y, z, size);
+      console.log(temp);
+      console.log(meshArray);
+      meshArray.push(temp);
+      scene.add(temp);
+    }
+  }
+  createMeshes(howManyMeshesExistOnStart, 0);
 
   //clock
   let clock = new THREE.Clock();
@@ -85,17 +105,54 @@ function main() {
     elapsedTimeDelta = clock.getDelta();
     const volume = microphone.getVolume();
     if (volume) {
-      if (torusKnot1 !== null) {
-        torusKnot1.rotation.z += 0.01;
-        torusKnot1.rotation.y += 0.005;
-        torusKnot1.scale.x = 1 + volume * 1.5;
-        torusKnot1.scale.y = 1 + volume * 1.5;
-        torusKnot1.scale.z = 1 + volume * 1.5;
+      if (camera !== null) {
+        camera.position.x += elapsedTimeDelta + volume;
+      }
+      if (light !== null) {
+        light.position.x += elapsedTimeDelta + volume;
       }
       if (icosahedron1 !== null) {
-        // TODO: Get the sphere to animate
-        icosahedron1.position.x = Math.sin(elapsedTimeDelta * 10);
-        icosahedron1.position.y = Math.sin(elapsedTimeDelta * 10);
+        icosahedron1.rotation.x += 0.05;
+        icosahedron1.rotation.y += 0.02;
+        icosahedron1.position.y = 10 * Math.sin(elapsedTime * 3);
+        icosahedron1.position.x += elapsedTimeDelta + volume;
+        icosahedron1.scale.y = 1 + volume;
+      }
+      if (
+        Math.ceil(elapsedTime) % howManySecoundsToAddMeshes === 0 &&
+        !calledTimeTemp // we only get into this if block once, since we append new objects into the scene only once per the specified interval in the "howManySecoundsToAddMeshes" variable
+      ) {
+        calledTimeTemp = true; // we remember, that we've been in that if, so we won't call it on the new screen refrest
+        for (let i = 0; i < howManyMeshesAreAddedAndRemoved; i++) {
+          if (meshArray.length >= maxMeshesAmount) {
+            // we start with a set amount of floral planes, but we add to that amount until we exceed a set threshold
+            scene.remove(meshArray.shift());
+          }
+          createMeshes(1, camera.position.z);
+          // console.log("time elapsed addition");
+        }
+      }
+      if (Math.floor(elapsedTime) % howManySecoundsToAddMeshes === 0) {
+        // HACK: we reset the calledTimeTemp variable, so we can add new objects when the new interval ends
+        calledTimeTemp = false;
+      }
+      if (
+        Math.ceil(camera.position.x) % howManyMetersToAddMeshes === 0 &&
+        !calledTimeTemp
+      ) {
+        calledTimeTemp = true;
+        for (let i = 0; i < howManyMeshesAreAddedAndRemoved; i++) {
+          if (meshArray.length >= maxMeshesAmount) {
+            // we start with a set amount of floral planes, but we add to that amount until we exceed a set threshold
+            scene.remove(meshArray.shift());
+          }
+          createMeshes(1, camera.position.x);
+          // console.log("distance elapsed addition");
+        }
+      }
+      if (Math.floor(elapsedTime) % howManySecoundsToAddMeshes === 0) {
+        // HACK: we reset the calledTimeTemp variable, so we can add new objects when the new interval ends
+        calledTimeTemp = false;
       }
     }
     renderer.render(scene, camera);
